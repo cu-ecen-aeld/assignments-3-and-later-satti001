63414aed98c7ba4da939b032f77b4bb617ef75e9
Salik Satti <34483524+satti001@users.noreply.github.com>
2023-03-20 15:20:01 +0500

    Update manual-linux.sh

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -52,10 +52,13 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
 
     echo "Finished building kernel image"
     
-    cp /tmp/aesd-autograder/linux-stable/arch/arm64/boot/Image /tmp/aesd-autograder
+   
 fi
 
 echo "Adding the Image in outdir"
+echo "Adding the Image in outdir"
+cd $OUTDIR
+cp -a linux-stable/arch/arm64/boot/Image ./
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -93,49 +96,87 @@ fi
 make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
 make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} CONFIG_PREFIX="${OUTDIR}/rootfs" install
 
+# Cross-compile sysroot dir
+CCSYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
+
+
 echo "Library dependencies"
 cd "${OUTDIR}/rootfs"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 
+# Interpreter / LIBS dependecies
+REQ_INTRP=$(${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter" | awk '{ gsub(/\[|\]/,"",$NF); print $NF}')
+REQ_LIBS=$(${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library" | awk '{ gsub(/\[|\]/,"",$NF); print $NF}')
+
 # TODO: Add library dependencies to rootfs
-cd "${OUTDIR}/rootfs"
-export SYSROOT=$(${CROSS_COMPILE}gcc --print-sysroot)
-cp -a $SYSROOT/lib/ld-linux-aarch64.so.1 lib
-cp -a $SYSROOT/lib64/ld-2.33.so lib64
-cp -a $SYSROOT/lib64/libm.so.6 lib64
-cp -a $SYSROOT/lib64/libm-2.33.so lib64
-cp -a $SYSROOT/lib64/libresolv.so.2 lib64
-cp -a $SYSROOT/lib64/libresolv-2.33.so lib64
-cp -a $SYSROOT/lib64/libc.so.6 lib64
-cp -a $SYSROOT/lib64/libc-2.33.so lib64
+for intrp in $REQ_INTRP
+do
+    echo "intrp: $intrp"
+	intrp=$(basename ${intrp})
+	LIB_SRC=$(find ${CCSYSROOT} -name $intrp)
+	LIB_TGT=$(realpath --no-symlinks --relative-to=$CCSYSROOT $LIB_SRC)
+	
+	echo "Copy $LIB_SRC to $LIB_TGT"
+	cp -a $LIB_SRC $LIB_TGT
+	
+    if [ -L $LIB_TGT ]; then
+		LNK_SRC=$(readlink -f $LIB_SRC)
+	    LNK_TGT=$(realpath --relative-to=$CCSYSROOT $LIB_SRC)
+		echo "Copy link: $LNK_SRC to $LNK_TGT"
+		cp -a $LNK_SRC $LNK_TGT
+    fi
+done
+
+
+for lib in $REQ_LIBS
+do
+    lib=$(basename ${lib})
+	LIB_SRC=$(find ${CCSYSROOT} -name $lib)
+	LIB_TGT=$(realpath --no-symlinks --relative-to=$CCSYSROOT $LIB_SRC)
+	echo "Copy $LIB_SRC to $LIB_TGT"
+	cp -a $LIB_SRC $LIB_TGT
+
+	LNKTGT=$(realpath --relative-to=$CCSYSROOT $LIB_SRC)
+	
+    # If src is link, copy link target locally
+    if [ -L $LIB_TGT ]; then
+		LNK_SRC=$(readlink -f $LIB_SRC)
+	    LNK_TGT=$(realpath --relative-to=$CCSYSROOT $LIB_SRC)
+		echo "Copy link: $LNK_SRC to $LNK_TGT"
+		cp -a $LNK_SRC $LNK_TGT
+    fi
+done
+
+cd ${ROOTFS_DIR}
 
 # TODO: Make device nodes
 sudo mknod -m 666 dev/null c 1 3
 sudo mknod -m 600 dev/console c 5 1
+
 # TODO: Clean and build the writer utility
 cd $FINDER_APP_DIR
 make clean
 make CROSS_COMPILE=$CROSS_COMPILE
+
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
-if [ -f writer ]
-then
-    cp writer $OUTDIR/rootfs/home/
-    cp finder.sh $OUTDIR/rootfs/home/
-    mkdir $OUTDIR/rootfs/home/conf/
-    cp conf/* $OUTDIR/rootfs/home/conf/
-    cp finder-test.sh $OUTDIR/rootfs/home/
-    cp autorun-qemu.sh $OUTDIR/rootfs/home/
-else
-    echo "Failed to cross-compile writer utility."
-    exit 1
-fi
+cp -a $FINDER_APP_DIR/writer $ROOTFS_DIR/home/
+echo -e "Copy finder app dir to rootfs/home"
+cp -a $FINDER_APP_DIR/finder.sh $ROOTFS_DIR/home/
+mkdir $ROOTFS_DIR/home/conf
+cp -a $FINDER_APP_DIR/conf/username.txt $ROOTFS_DIR/home/conf/
+cp -a $FINDER_APP_DIR/finder-test.sh $ROOTFS_DIR/home/
+cp -a $FINDER_APP_DIR/autorun-qemu.sh $ROOTFS_DIR/home/
+
+
 # TODO: Chown the root directory
 cd "${OUTDIR}/rootfs"
 sudo chown -R root:root *
 # TODO: Create initramfs.cpio.gz
+
+cd $ROOTFS_DIR
 find . | cpio -H newc -ov --owner root:root > ../initramfs.cpio
 cd ..
 gzip -f initramfs.cpio
